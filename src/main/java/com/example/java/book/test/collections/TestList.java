1ac2e34b5d9ce53bf8002b99a5282bf35f09f514
Gen <405822000@qq.com>
2020-05-08 14:51:44 +0800

    note update

---
+++ src/main/java/com/example/java/book/test/collections/TestList.java
@@ -0,0 +1,43 @@
+package com.example.java.book.test.collections;
+
+import java.util.*;
+
+/**
+ * @author Gen
+ * @date 7/5/2020
+ * @time 下午 3:21
+ * Descriptioin No Descriptioin
+ */
+public class TestList {
+
+
+
+    /**
+     * Descriptioin :
+     * @param i 12
+     * @param j 32
+     * @return java.lang.String
+     * @author GenYu
+     * @date 7/5/2020 下午 3:50
+     */
+    public String test(int i, int j){
+        List<String> list = new ArrayList<>(10);
+
+
+        Stack<String> stack = new Stack<>();
+
+        TreeMap<Integer, String> treeMap = new TreeMap<>();
+
+
+        HashMap<Integer, String> hashMap = new HashMap<>(10);
+
+
+
+        return null;
+    }
+    
+    
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/example/java/book/test/lambda/TestLambda.java
@@ -0,0 +1,37 @@
+package com.example.java.book.test.lambda;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author Gen
+ * @date 8/5/2020
+ * @time 下午 1:31
+ * Descriptioin 练习Lambda
+ */
+public class TestLambda {
+
+    public static void main(String[] args) {
+        String[] atp = {"Rafael Nadal", "Novak Djokovic",
+                "Stanislas Wawrinka",
+                "David Ferrer","Roger Federer",
+                "Andy Murray","Tomas Berdych",
+                "Juan Martin Del Potro"};
+        List<String> players =  Arrays.asList(atp);
+
+        // 普通遍历
+        for (String player : players) {
+            System.out.println("player = " + player);
+        }
+
+        //Lambda表达式遍历
+        players.forEach((player) -> System.out.println("player = " + player));
+
+        //使用了::遍历
+        players.forEach(System.out::println);
+
+    }
+
+
+
+}
